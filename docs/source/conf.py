9df0d1eb6d6915d2636a9d2446a7567c308bce38
Florentin Dörre <florentin.dorre@neo4j.com>
2025-01-10 16:38:57 +0100

    Format code

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,15 +6,13 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-from pathlib import Path
 import pathlib
-import sys
 
 
-project = 'Graph Visualization for Python'
-copyright = '2025, Neo4j, Inc.'
-author = 'Neo4j, Inc.'
-release = '0.1.6'
+project = "Graph Visualization for Python"
+copyright = "2025, Neo4j, Inc."
+author = "Neo4j, Inc."
+release = "0.1.6"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -25,9 +23,8 @@ extensions = [
     "sphinx.ext.napoleon",  # Support for NumPy and Google style docstrings
 ]
 
-templates_path = ['_templates']
-exclude_patterns = []
-
+templates_path = ["_templates"]
+exclude_patterns: list[str] = []
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -35,16 +32,18 @@ exclude_patterns = []
 
 
 # use neo4j theme, which extends neo4j docs css for sphinx
-html_theme = 'neo4j'
+html_theme = "neo4j"
 theme_path = pathlib.Path(__file__).resolve().parent
 print(theme_path)
 html_static_path = [str(theme_path / "themes")]
+
 
 # 01-nav.js is a copy of a js file of the same name that is included in the docs-ui bundle
 def setup(app):  # type: ignore
     app.add_js_file("https://neo4j.com/docs/assets/js/site.js", loading_method="defer")
     app.add_js_file("js/12-fragment-jumper.js", loading_method="defer")
     app.add_js_file("js/deprecated.js", loading_method="defer")
+
 
 rst_epilog = """
 .. |api-version| replace:: {versionnum}
