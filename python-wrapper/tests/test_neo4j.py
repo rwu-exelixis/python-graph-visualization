ba3382430a0bb6c5bca70ca222b037a45cec9700
Adam Schill Collberg <adam.schill.collberg@protonmail.com>
2025-03-31 11:00:21 +0200

    Fix issue of conflicting keys when importing from Neo4j

--- python-wrapper/tests/test_neo4j.py
+++ python-wrapper/tests/test_neo4j.py
@@ -1,5 +1,6 @@
 from typing import Generator
 
+import neo4j
 import pytest
 from neo4j import Session
 
@@ -10,7 +11,8 @@ from neo4j_viz.node import Node
 @pytest.fixture(scope="class", autouse=True)
 def graph_setup(neo4j_session: Session) -> Generator[None, None, None]:
     neo4j_session.run(
-        "CREATE (a:_CI_A {name:'Alice', height:20})-[:KNOWS {year: 2025}]->(b:_CI_A:_CI_B {name:'Bob', height:10}), (b)-[:RELATED {year: 2015}]->(a)"
+        "CREATE (a:_CI_A {name:'Alice', height:20, id:42, _id: 1337, caption: 'hello'})-[:KNOWS {year: 2025, id: 41, source: 1, target: 2}]->"
+        "(b:_CI_A:_CI_B {name:'Bob', height:10, id: 84, size: 11, labels: [1,2]}), (b)-[:RELATED {year: 2015, _type: 'A', caption:'hej'}]->(a)"
     )
     yield
     neo4j_session.run("MATCH (n:_CI_A|_CI_B) DETACH DELETE n")
@@ -22,11 +24,30 @@ def test_from_neo4j_graph(neo4j_session: Session) -> None:
 
     VG = from_neo4j(graph)
 
-    node_ids: list[str] = [node.element_id for node in graph.nodes]
+    sorted_nodes: list[neo4j.graph.Node] = sorted(graph.nodes, key=lambda x: dict(x.items())["name"])
+    node_ids: list[str] = [node.element_id for node in sorted_nodes]
 
     expected_nodes = [
-        Node(id=node_ids[0], caption="_CI_A", labels=["_CI_A"], name="Alice", height=20),
-        Node(id=node_ids[1], caption="_CI_A:_CI_B", labels=["_CI_A", "_CI_B"], name="Bob", height=10),
+        Node(
+            id=node_ids[0],
+            caption="_CI_A",
+            labels=["_CI_A"],
+            name="Alice",
+            height=20,
+            __id=42,
+            _id=1337,
+            __caption="hello",
+        ),
+        Node(
+            id=node_ids[1],
+            caption="_CI_A:_CI_B",
+            labels=["_CI_A", "_CI_B"],
+            name="Bob",
+            height=10,
+            __id=84,
+            __size=11,
+            __labels=[1, 2],
+        ),
     ]
 
     assert len(VG.nodes) == 2
@@ -47,11 +68,31 @@ def test_from_neo4j_result(neo4j_session: Session) -> None:
     VG = from_neo4j(result)
 
     graph = result.graph()
-    node_ids: list[str] = [node.element_id for node in graph.nodes]
+
+    sorted_nodes: list[neo4j.graph.Node] = sorted(graph.nodes, key=lambda x: dict(x.items())["name"])
+    node_ids: list[str] = [node.element_id for node in sorted_nodes]
 
     expected_nodes = [
-        Node(id=node_ids[0], caption="_CI_A", labels=["_CI_A"], name="Alice", height=20),
-        Node(id=node_ids[1], caption="_CI_A:_CI_B", labels=["_CI_A", "_CI_B"], name="Bob", height=10),
+        Node(
+            id=node_ids[0],
+            caption="_CI_A",
+            labels=["_CI_A"],
+            name="Alice",
+            height=20,
+            __id=42,
+            _id=1337,
+            __caption="hello",
+        ),
+        Node(
+            id=node_ids[1],
+            caption="_CI_A:_CI_B",
+            labels=["_CI_A", "_CI_B"],
+            name="Bob",
+            height=10,
+            __id=84,
+            __size=11,
+            __labels=[1, 2],
+        ),
     ]
 
     assert len(VG.nodes) == 2
@@ -71,11 +112,32 @@ def test_from_neo4j_graph_full(neo4j_session: Session) -> None:
 
     VG = from_neo4j(graph, node_caption="name", relationship_caption="year", size_property="height")
 
-    node_ids: list[str] = [node.element_id for node in graph.nodes]
+    sorted_nodes: list[neo4j.graph.Node] = sorted(graph.nodes, key=lambda x: dict(x.items())["name"])
+    node_ids: list[str] = [node.element_id for node in sorted_nodes]
 
     expected_nodes = [
-        Node(id=node_ids[0], caption="Alice", labels=["_CI_A"], name="Alice", height=20, size=60.0),
-        Node(id=node_ids[1], caption="Bob", labels=["_CI_A", "_CI_B"], name="Bob", height=10, size=3.0),
+        Node(
+            id=node_ids[0],
+            caption="Alice",
+            labels=["_CI_A"],
+            name="Alice",
+            height=20,
+            size=60.0,
+            __id=42,
+            _id=1337,
+            __caption="hello",
+        ),
+        Node(
+            id=node_ids[1],
+            caption="Bob",
+            labels=["_CI_A", "_CI_B"],
+            name="Bob",
+            height=10,
+            size=3.0,
+            __id=84,
+            __size=11,
+            __labels=[1, 2],
+        ),
     ]
 
     assert len(VG.nodes) == 2
