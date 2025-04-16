2db5612d28e3bc873f807582e3d24fea00d1a823
Adam Schill Collberg <adam.schill.collberg@protonmail.com>
2025-04-16 15:12:43 +0200

    Update color_nodes signature

--- python-wrapper/src/neo4j_viz/colors.py
+++ python-wrapper/src/neo4j_viz/colors.py
@@ -7,7 +7,7 @@ from pydantic_extra_types.color import ColorType
 ColorsType = Union[dict[Any, ColorType], Iterable[ColorType]]
 
 
-class PropertyType(Enum):
+class ColorSpace(Enum):
     DISCRETE = "discrete"
     CONTINUOUS = "continuous"
 
