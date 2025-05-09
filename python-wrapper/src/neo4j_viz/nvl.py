0af88a4096d1200e813e15db62831ddb8f4671f1
Adam Schill Collberg <adam.schill.collberg@protonmail.com>
2025-05-09 11:57:53 +0200

    Use new button design
    Co-Authored-By: Florentin Dörre <florentin.dorre@neotechnology.com>

--- python-wrapper/src/neo4j_viz/nvl.py
+++ python-wrapper/src/neo4j_viz/nvl.py
@@ -51,7 +51,7 @@ class NVL:
 
         if show_hover_tooltip:
             hover_element = f"document.getElementById('{container_id}-tooltip')"
-            hover_div = f'<div id="{container_id}-tooltip" style="width: 20%; min-width: 100px; max-width: 600px; max-height: 80%; position: absolute; z-index: 2147483647; right: 0; bottom: 0; background: white; display: none; border: solid; border-color: #BBBEC3; border-width: 0.5px; padding: 0.8rem; border-radius: 8px; margin-bottom: 1rem; margin-right: 0.5rem; filter: drop-shadow(0 4px 8px rgba(26,27,29,0.12)); font-family: PublicSans; color: #4D5157; font-size: 14px"></div>'
+            hover_div = f'<div id="{container_id}-tooltip" style="width: 20%; min-width: 100px; max-width: 600px; max-height: 80%; position: absolute; z-index: 2147483647; right: 0; bottom: 0; background: inherit; display: none; border: solid; border-color: #BBBEC3; border-width: 0.5px; padding: 0.8rem; border-radius: 8px; margin-bottom: 1rem; margin-right: 0.5rem; filter: drop-shadow(0 4px 8px rgba(26,27,29,0.12)); font-family: PublicSans; color: #aeaeae; font-size: 14px"></div>'
         else:
             hover_element = "null"
             hover_div = ""
@@ -73,39 +73,55 @@ class NVL:
         """
         full_code = self.library_code + js_code
 
-        # Although a `<style>` element within the `HTML` block would work,
-        # it is not standard to use it outside a `<head>` element.
-        # For this reason, the style for the buttons is inlined here.
-        # The combination of `onmouseover` and `onmouseout` replaces the
-        # `button:hover` descriptor.
-        button_style = """
-            background-color: #0a6190;
-            color: #fff;
-            padding: .85rem 1rem;
-            border: 1px solid transparent;
-            border-radius: 5px;
-            text-align: center;
-            font-family: 'Public Sans', 'Helvetica Neue', helvetica, roboto, arial, sans-serif;
-            font-weight: 500;
-            font-size: 14px;
-        """
-        button_attrs = f"""
-            style="{button_style}"
-            onmouseover="this.style.backgroundColor='#014063'"
-            onmouseout="this.style.backgroundColor='#0a6190'"
-        """
+        base_folder = files("neo4j_viz")
+        resource_folder = base_folder / "resources"
+
+        zoom_in_path = resource_folder / "zoom-in.svg"
+        with zoom_in_path.open("r", encoding="utf-8") as file:
+            zoom_in_svg = file.read()
+
+        zoom_out_path = resource_folder / "zoom-out.svg"
+        with zoom_out_path.open("r", encoding="utf-8") as file:
+            zoom_out_svg = file.read()
+
+        screenshot_path = resource_folder / "screenshot.svg"
+        with screenshot_path.open("r", encoding="utf-8") as file:
+            screenshot_svg = file.read()
+
+        css = resource_folder / "styles.css"
+        with css.open("r", encoding="utf-8") as file:
+            css_data = file.read()
 
         html_output = f"""
-        <div style="position: absolute; z-index: 2147483647; left: 0; top: 0; padding: 1rem">
-            <button type="button" {button_attrs} onclick="{nvl_varname}.nvl.saveToFile({{ filename: '{download_name}' }})">Save screenshot</button>
-            <button type="button" {button_attrs} onclick="{nvl_varname}.nvl.setZoom({nvl_varname}.nvl.getScale() + 0.1)">Zoom in</button>
-            <button type="button" {button_attrs} onclick="{nvl_varname}.nvl.setZoom({nvl_varname}.nvl.getScale() - 0.1)">Zoom out</button> 
+        <style>
+            {css_data}
+        </style>
+        <div style="position: absolute; z-index: 2147483647; right: 0; top: 0; padding: 1rem">
+            <button type="button" onclick="{nvl_varname}.nvl.saveToFile({{ filename: '{download_name}' }})" class="icon">
+                {screenshot_svg}
+            </button>
+            <button type="button" onclick="{nvl_varname}.nvl.setZoom({nvl_varname}.nvl.getScale() + 0.5)" class="icon">
+                {zoom_in_svg}
+            </button>
+            <button type="button" onclick="{nvl_varname}.nvl.setZoom({nvl_varname}.nvl.getScale() - 0.5)" class="icon">
+                {zoom_out_svg}
+            </button>
         </div>
         <div id="{container_id}" style="width: {width}; height: {height}; position: relative;">
             {hover_div}
         </div>
+
         <script>
+            getTheme = () => {{
+                const backgroundColorString = window.getComputedStyle(document.body, null).getPropertyValue('background-color')
+                const colorsArray = backgroundColorString.match(/\d+/g);
+                const brightness = Number(colorsArray[0]) * 0.2126 + Number(colorsArray[1]) * 0.7152 + Number(colorsArray[2]) * 0.0722
+                return brightness < 128 ? "dark" : "light"
+            }}
+            document.documentElement.className = getTheme()
+
             {full_code}
         </script>
         """
+
         return HTML(html_output)  # type: ignore[no-untyped-call]
