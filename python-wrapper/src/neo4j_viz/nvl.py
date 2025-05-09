6e8390f78e24f86cb2d0e2888ff3e16d5e8d6ee6
Adam Schill Collberg <adam.schill.collberg@protonmail.com>
2025-05-09 17:29:24 +0200

    Refactor and improve button styling
    Co-Authored-By: Florentin Dörre <florentin.dorre@neotechnology.com>

--- python-wrapper/src/neo4j_viz/nvl.py
+++ python-wrapper/src/neo4j_viz/nvl.py
@@ -19,9 +19,26 @@ class NVL:
         nvl_entry_point = resource_folder / "nvl_entrypoint"
 
         js_path = nvl_entry_point / "base.js"
-
         with js_path.open("r", encoding="utf-8") as file:
             self.library_code = file.read()
+
+        styles_path = nvl_entry_point / "styles.css"
+        with styles_path.open("r", encoding="utf-8") as file:
+            self.styles = file.read()
+
+        icons = resource_folder / "icons"
+
+        zoom_in_path = icons / "zoom-in.svg"
+        with zoom_in_path.open("r", encoding="utf-8") as file:
+            self.zoom_in_svg = file.read()
+
+        zoom_out_path = icons / "zoom-out.svg"
+        with zoom_out_path.open("r", encoding="utf-8") as file:
+            self.zoom_out_svg = file.read()
+
+        screenshot_path = icons / "screenshot.svg"
+        with screenshot_path.open("r", encoding="utf-8") as file:
+            self.screenshot_svg = file.read()
 
     def unsupported_field_type_error(self, e: TypeError, entity: str) -> Exception:
         if "not JSON serializable" in str(e):
@@ -51,7 +68,7 @@ class NVL:
 
         if show_hover_tooltip:
             hover_element = f"document.getElementById('{container_id}-tooltip')"
-            hover_div = f'<div id="{container_id}-tooltip" style="width: 20%; min-width: 100px; max-width: 600px; max-height: 80%; position: absolute; z-index: 2147483647; right: 0; bottom: 0; background: inherit; display: none; border: solid; border-color: #BBBEC3; border-width: 0.5px; padding: 0.8rem; border-radius: 8px; margin-bottom: 1rem; margin-right: 0.5rem; filter: drop-shadow(0 4px 8px rgba(26,27,29,0.12)); font-family: PublicSans; color: #aeaeae; font-size: 14px"></div>'
+            hover_div = f'<div id="{container_id}-tooltip" class="tooltip" style="display: none;"/>'
         else:
             hover_element = "null"
             hover_div = ""
@@ -73,38 +90,19 @@ class NVL:
         """
         full_code = self.library_code + js_code
 
-        base_folder = files("neo4j_viz")
-        resource_folder = base_folder / "resources"
-
-        zoom_in_path = resource_folder / "zoom-in.svg"
-        with zoom_in_path.open("r", encoding="utf-8") as file:
-            zoom_in_svg = file.read()
-
-        zoom_out_path = resource_folder / "zoom-out.svg"
-        with zoom_out_path.open("r", encoding="utf-8") as file:
-            zoom_out_svg = file.read()
-
-        screenshot_path = resource_folder / "screenshot.svg"
-        with screenshot_path.open("r", encoding="utf-8") as file:
-            screenshot_svg = file.read()
-
-        css = resource_folder / "styles.css"
-        with css.open("r", encoding="utf-8") as file:
-            css_data = file.read()
-
         html_output = f"""
         <style>
-            {css_data}
+            {self.styles}
         </style>
         <div style="position: absolute; z-index: 2147483647; right: 0; top: 0; padding: 1rem">
-            <button type="button" onclick="{nvl_varname}.nvl.saveToFile({{ filename: '{download_name}' }})" class="icon">
-                {screenshot_svg}
+            <button type="button" title="Save as PNG" onclick="{nvl_varname}.nvl.saveToFile({{ filename: '{download_name}' }})" class="icon">
+                {self.screenshot_svg}
             </button>
-            <button type="button" onclick="{nvl_varname}.nvl.setZoom({nvl_varname}.nvl.getScale() + 0.5)" class="icon">
-                {zoom_in_svg}
+            <button type="button" title="Zoom in" onclick="{nvl_varname}.nvl.setZoom({nvl_varname}.nvl.getScale() + 0.5)" class="icon">
+                {self.zoom_in_svg}
             </button>
-            <button type="button" onclick="{nvl_varname}.nvl.setZoom({nvl_varname}.nvl.getScale() - 0.5)" class="icon">
-                {zoom_out_svg}
+            <button type="button" title="Zoom out" onclick="{nvl_varname}.nvl.setZoom({nvl_varname}.nvl.getScale() - 0.5)" class="icon">
+                {self.zoom_out_svg}
             </button>
         </div>
         <div id="{container_id}" style="width: {width}; height: {height}; position: relative;">
