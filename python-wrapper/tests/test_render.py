99fbbe4dc99930cfbf5b2743133b86955d479a3d
Adam Schill Collberg <adam.schill.collberg@protonmail.com>
2025-05-22 11:16:51 +0200

    Convert non-json-serializable properties to strings

--- python-wrapper/tests/test_render.py
+++ python-wrapper/tests/test_render.py
@@ -64,34 +64,6 @@ def test_basic_render(render_option: dict[str, Any], tmp_path: Path) -> None:
     assert not severe_logs, f"Severe logs found: {severe_logs}, all logs: {logs}"
 
 
-def test_unsupported_field_type() -> None:
-    with pytest.raises(
-        ValueError, match="A field of a node object is not supported: Object of type set is not JSON serializable"
-    ):
-        nodes = [
-            Node(
-                id="4:d09f48a4-5fca-421d-921d-a30a896c604d:0", caption="Person", properties={"unsupported": {1, 2, 3}}
-            ),
-        ]
-        VG = VisualizationGraph(nodes=nodes, relationships=[])
-        VG.render()
-
-    with pytest.raises(
-        ValueError,
-        match="A field of a relationship object is not supported: Object of type set is not JSON serializable",
-    ):
-        relationships = [
-            Relationship(
-                source="4:d09f48a4-5fca-421d-921d-a30a896c604d:0",
-                target="4:d09f48a4-5fca-421d-921d-a30a896c604d:6",
-                caption="BUYS",
-                properties={"unsupported": {1, 2, 3}},
-            ),
-        ]
-        VG = VisualizationGraph(nodes=[], relationships=relationships)
-        VG.render()
-
-
 def test_max_allowed_nodes_limit() -> None:
     nodes = [Node(id=i) for i in range(10_001)]
     VG = VisualizationGraph(nodes=nodes, relationships=[])
@@ -121,3 +93,20 @@ def test_render_warnings() -> None:
         "relationships. If you need these features, use the canvas renderer by setting the `renderer` parameter",
     ):
         VG.render(max_allowed_nodes=20_000, renderer=Renderer.WEB_GL)
+
+
+def test_render_non_json_serializable() -> None:
+    import datetime
+
+    nodes = [
+        Node(
+            id=0,
+            properties={
+                "non-json-serializable": datetime.datetime.now(),
+            },
+        )
+    ]
+
+    VG = VisualizationGraph(nodes=nodes, relationships=[])
+
+    VG.render()
