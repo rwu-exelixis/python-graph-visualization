d020ed4db1983745ecc8d56e03ea44c4967a2bc5
Adam Schill Collberg <adam.schill.collberg@protonmail.com>
2025-05-23 15:13:01 +0200

    Improve `from_dfs` error messages
    Co-Authored-By: Florentin Dörre <florentin.dorre@neotechnology.com>

--- python-wrapper/tests/test_pandas.py
+++ python-wrapper/tests/test_pandas.py
@@ -1,3 +1,4 @@
+import pytest
 from pandas import DataFrame
 from pydantic_extra_types.color import Color
 
@@ -130,3 +131,62 @@ def test_from_dfs() -> None:
     assert VG.relationships[1].source == 1
     assert VG.relationships[1].target == 0
     assert VG.relationships[1].caption == "REL2"
+
+
+def test_node_errors() -> None:
+    nodes = DataFrame(
+        {"caption": ["A", "B"], "size": [1337, 42], "color": "#FF0000", "instrument": ["piano", "guitar"]}
+    )
+    with pytest.raises(
+        ValueError,
+        match=r"Mandatory node column 'id' is missing. Expected one of \['id', 'ID', 'id', 'nodeid', 'NODEID', 'nodeid', 'node_id', 'NODE_ID', 'nodeId'\] to be present",
+    ):
+        from_dfs(nodes, [])
+
+    nodes = DataFrame(
+        {
+            "id": [0, 1],
+            "caption": ["A", "B"],
+            "size": ["aaa", 42],
+            "color": "#FF0000",
+            "instrument": ["piano", "guitar"],
+        }
+    )
+    with pytest.raises(
+        ValueError,
+        match=r"Error for node column 'size' with provided input 'aaa'. Reason: Input should be a valid integer, unable to parse string as an integer",
+    ):
+        from_dfs(nodes, [])
+
+
+def test_rel_errors() -> None:
+    nodes = DataFrame(
+        {"id": [0, 1], "caption": ["A", "B"], "size": [1337, 42], "color": "#FF0000", "instrument": ["piano", "guitar"]}
+    )
+    relationships = DataFrame(
+        {
+            "target": [1, 0],
+            "caption": ["REL", "REL2"],
+            "weight": [1.0, 2.0],
+        }
+    )
+    with pytest.raises(
+        ValueError,
+        match=r"Mandatory relationship column 'source' is missing. Expected one of \['source', 'SOURCE', 'source', 'sourcenodeid', 'SOURCENODEID', 'sourcenodeid', 'source_node_id', 'SOURCE_NODE_ID', 'sourceNodeId', 'from', 'FROM', 'from'\] to be present",
+    ):
+        from_dfs(nodes, relationships)
+
+    relationships = DataFrame(
+        {
+            "source": [0, 1],
+            "target": [1, 0],
+            "caption": ["REL", "REL2"],
+            "caption_size": [1.0, -300],
+            "weight": [1.0, 2.0],
+        }
+    )
+    with pytest.raises(
+        ValueError,
+        match=r"Error for relationship column 'caption_size' with provided input '-300.0'. Reason: Input should be greater than 0",
+    ):
+        from_dfs(nodes, relationships)
